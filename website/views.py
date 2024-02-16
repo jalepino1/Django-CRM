1505980a7d3eecd89e689dda32241858bb3253c8
Jalepino1 <hengeno818@gmail.com>
2024-02-16 16:20:07 -0600

    register-user

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from . forms import SignUpForm
 # Create your views here.
 #request are request that are sent from interaction with the webpage and are sent to the backend to process them
 def home(request):
@@ -27,5 +28,20 @@ def logout_user(request):
     messages.success(request, 'You have been logged out')
     return redirect('home')
 
+# request.method is looking for a post from the url request 
+# register, if there is a post, it will define form as signupform(request.post) which gets the post from user and sends the sign up form
+
 def register_user(request):
-    pass
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid:
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully created an account.")
+            return redirect('home')
+        else:
+            form = SignUpForm()
+            return render(request, 'register.html', {'form' : form })
