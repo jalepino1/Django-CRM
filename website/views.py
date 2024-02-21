2c6739d921148eb78b9c05191a56f4989e4aa8be
Jalepino1 <hengeno818@gmail.com>
2024-02-21 16:47:03 -0600

    record selection added

--- website/views.py
+++ website/views.py
@@ -50,4 +50,14 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form' : form })
     
-    return render(request, 'register.html', {'form' : form })
+    return render(request, 'register.html')
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record' : customer_record })
+    else:
+        messages.success(request, "User not logged in.")
+        return redirect('home')
+
